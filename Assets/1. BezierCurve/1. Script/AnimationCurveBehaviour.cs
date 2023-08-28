cae9c5ef93ca8e11b3d7dc5e2d804a07176cc62f
rbase0218 <rbase@naver.com>
2023-08-28 18:56:34 +0900

    Feat : AnimationCurve를 활용한 움직임 구현

---
+++ Assets/1. BezierCurve/1. Script/AnimationCurveBehaviour.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class AnimationCurveBehaviour : MonoBehaviour
+{
+    public AnimationCurve animCurve;
+    public GameObject animObject;
+    public Vector3 animTargetPos;
+
+    [Space(10)] public GameObject normalObject;
+    public Vector3 normalTargetPos;
+
+    [Space(10)] public float targetOffset = .0f;
+    public bool isStarted = false;
+
+
+    [Button("Set Target Pos")]
+    private void SetTargetPos()
+    {
+        animTargetPos = animObject.transform.position;
+        animTargetPos.x += targetOffset;
+
+        normalTargetPos = normalObject.transform.position;
+        normalTargetPos.x += targetOffset;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A) || isStarted)
+        {
+            isStarted = true;
+
+            MoveNormal();
+        }
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            StartCoroutine(MoveAnimCurve());
+        }
+    }
+
+    private void MoveNormal()
+    {
+        if (Vector3.Distance(normalObject.transform.position, normalTargetPos) > 0.1f)
+        {
+            normalObject.transform.Translate(Vector3.right * 15f * Time.deltaTime);
+        }
+    }
+
+    private IEnumerator MoveAnimCurve()
+    {
+        float timer = .0f;
+
+        while (timer < 1f)
+        {
+            animObject.transform.position = new Vector3(animCurve.Evaluate(timer) * animTargetPos.x,
+                animObject.transform.position.y,
+                animObject.transform.position.z);
+
+            yield return null;
+
+            timer += Time.deltaTime;
+        }
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(animTargetPos, 0.1f);
+        Gizmos.DrawLine(animObject.transform.position, animTargetPos);
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(normalTargetPos, 0.1f);
+        Gizmos.DrawLine(normalObject.transform.position, normalTargetPos);
+    }
+}
--- Assets/1. BezierCurve/1. Script/BezierCurveBehaviour.cs
+++ Assets/1. BezierCurve/1. Script/BezierCurveBehaviour.cs
@@ -2,17 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class BezierPointData
+{
+    public Vector3 startPoint;
+    public Vector3 endPoint;
+    public Vector3 startTangent;
+    public Vector3 endTangent;
+}
+
 public class BezierCurveBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public List<BezierPointData> bezierPointDatas;
+    public bool isShow;
 
-    // Update is called once per frame
-    void Update()
+    public void CreatePoint()
     {
-        
+        for (int i = 0; i < bezierPointDatas.Count - 1; ++i)
+        {
+            bezierPointDatas[i].startPoint = new Vector3(i, i, i);
+            bezierPointDatas[i].endPoint = new Vector3(i + 1, i + 1, i + 1);
+            bezierPointDatas[i].startTangent = Vector3.zero;
+            bezierPointDatas[i].endTangent = Vector3.zero;
+        }
     }
 }
+
